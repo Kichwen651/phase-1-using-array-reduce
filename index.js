1a3f9febbb1706353c88bfc559b468c7a680dbf8
Victor kichwen <victor.kichwen@student.moringaschool.com>
2024-10-01 22:46:07 +0300

    complete lab 11

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// const batteryBatches = [4, 5, 3, 4, 4,];
+ 
+// const totalBatteries = batteryBatches.reduce((total, batch) =>{
+//     return total + batch;
+// }, 0);
+// console.log(totalBatteries);
+  
+
+// const batteryBatches = [4, 5, 3, 4, 4];
+
+// const totalBatteries = batteryBatches.reduce((total, batch) => {
+//     return total + batch;
+// }, 0);
+
+// console.log("Total Assembled Batteries:", totalBatteries); // Output: Total Assembled Batteries: 20
+
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+const totalBatteries = batteryBatches.reduce((total, batch) =>{
+    return total + batch;
+}, 0); // Starting the total at 0
+
+console.log(totalBatteries); // Output: 20
 
 // Code your solution here
